bf229315c64dbf24a7d70a05e3f65427dd632152
Matt Petty <matt@kizmeta.com>
2010-01-20 15:27:46 -0600

    Fix path

--- app/controllers/sparks_controller.rb
+++ app/controllers/sparks_controller.rb
@@ -7,7 +7,7 @@ class SparksController < ApplicationController
     params[:format] = $1
  
     static_file = params[:sparkpath].join('/')
-    if Dir.glob("#{Rails.root}/app/views/sparks/#{static_file}.*").empty?
+    if Dir.glob("#{Rails.root}/app/views/static/#{static_file}.*").empty?
       render :file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404
     else
       render :action => static_file and return
